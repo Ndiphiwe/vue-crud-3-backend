e7a1686f3bbc921e4e2c664c32039d7c440eb258
Ndiphiwe Ngxola <pndira1770@gmail.com>
2022-02-15 15:38:32 +0200

    added files

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    HOST: "us-cdbr-iron-east-02.cleardb.net",
-    USER: "b7e2437887xxxa",
-    PASSWORD: "0200xxx6",
-    DB: "heroku_7643ec736354xxx",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: "localhost",
+  USER: "ndiphiwe",
+  PASSWORD: "SeykNWYbuSW@6qbv",
+  DB: "tutorialdb",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var corsOptions = {
 // db.sequelize.sync({ force: true }).then(() => {
 //     console.log("Drop and re-sync db.");
 //   });
-app.use(cors(corsOptions));
+app.use(cors());
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 });
 require("./app/routes/turorial.routes")(app);
 // set port, listen for requests
-const PORT = process.env.PORT || 8040;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
